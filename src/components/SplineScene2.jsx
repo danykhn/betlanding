e74419d7cad522f135391e4fe21313390ccf8e9c
danykhn <danjel2014@gmail.com>
2025-12-03 18:37:01 -0300

    add spline

---
+++ src/components/SplineScene2.jsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineScene2() {
+  return (
+    <div className="w-full h-full">
+      <Spline scene="https://prod.spline.design/v9K2Yx8shXCdi634/scene.splinecode" />   </div>
+  );
+}
