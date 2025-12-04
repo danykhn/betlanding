b270fa61a4427123ba27f1e9e42e0c87992511c1
danykhn <danjel2014@gmail.com>
2025-12-04 17:36:45 -0300

    sdd

--- src/components/SplineScene2.jsx
+++ src/components/SplineScene2.jsx
@@ -3,6 +3,6 @@ import Spline from '@splinetool/react-spline';
 export default function SplineScene2() {
   return (
     <div className="w-full h-full">
-      <Spline scene="https://prod.spline.design/v9K2Yx8shXCdi634/scene.splinecode" />   </div>
+      <Spline scene="https://prod.spline.design/VxIsvDRvApmtWmWE/scene.splinecode" />   </div>
   );
 }
